d6f619d64a350d3e04f372d0a7b4b6019944295c
jungwoogyun <jwg8910@naver.com>
2024-02-29 13:10:42 +0900

    1

--- src/Ch07/C00While_test2.java
+++ src/Ch07/C00While_test2.java
@@ -20,22 +20,38 @@ public class C00While_test2 {
 //			dan++;
 //	
 //		}
-		//구구단 출력(9단 - 2단)
-		int dan=2;
+//		//구구단 출력(9단 - 2단)
+//		int dan=9;
+//		int i;
+//		while(dan>=2) {
+//			
+//			i=9;
+//			while(i>=1) {
+//				
+//				System.out.printf("%d x %d = %d\n",dan,i,dan*i);
+//				i--;
+//			}
+//			System.out.println();
+//			dan--;
+//	
+//		}
+		
+		//구구단 출력(2단 - N단)
+		//N은 Scanner로 받습니다.
+		int dan=9;
 		int i;
-		while(dan<=9) {
+		while(dan>=2) {
 			
-			i=1;
-			while(i<=9) {
+			i=9;
+			while(i>=1) {
 				
 				System.out.printf("%d x %d = %d\n",dan,i,dan*i);
-				i++;
+				i--;
 			}
 			System.out.println();
-			dan++;
+			dan--;
 	
 		}	
-		
 		
 	}
 
