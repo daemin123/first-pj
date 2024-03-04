f7dd03f2e0f130761dfaed102be7a17a8ff10565
jungwoogyun <jwg8910@naver.com>
2024-03-04 12:36:13 +0900

    .

--- src/Ch07/C03BreakContinue.java
+++ src/Ch07/C03BreakContinue.java
@@ -9,7 +9,6 @@ public class C03BreakContinue {
 	
 		//1 break
 		// -1을 입력하기 전까지 계속 정수값을 받아 누적 합을 출력
-
 //		Scanner sc = new Scanner(System.in);
 //		int n;
 //		int sum=0;
@@ -46,6 +45,7 @@ public class C03BreakContinue {
 //			System.out.println();
 //			dan++;
 //		}
+//		
 		
 //		int dan =2;
 //		int i = 1;
@@ -72,19 +72,19 @@ public class C03BreakContinue {
 		//2 continue
 		// 1부터 10까지의 수중에 3의 배수를 제외한 합 출력
 		
-//		int i=1;
-//		int sum=0;
-//		while(i<=10)
-//		{
-//			if(i%3==0) {	
-//				i++;
-//				continue; //근접한 반복문의 조건식으로 돌아감
-//			}
-//			System.out.println("i=" + i);
-//			sum+=i;
-//			i++;
-//			
-//		}
+		int i=1;
+		int sum=0;
+		while(i<=10)
+		{
+			if(i%3==0) {
+				i++;
+				continue; //근접한 반복문의 조건식으로 돌아감
+			}
+			System.out.println("i=" + i);
+			sum+=i;
+			i++;
+			
+		}
 //		System.out.println("SUM = " + sum);
 		
 	}
---
+++ src/Ch99/test.java
@@ -0,0 +1,70 @@
+package Ch99;
+
+
+class Buyer{
+	int myMoney;
+	int appleCnt;
+	Buyer(int money,int cnt){
+		this.myMoney = money;
+		this.appleCnt = cnt;
+	}
+	
+	public void pay(Seller seller, int money) {
+		myMoney-=money; 					// 1 내 myMoney에서 money만큼 차감
+		int cnt=seller.receive(money);		// 2 seller의 receive메서드를 이용해서 money를 전달
+		appleCnt+=cnt;						// 6 사과개수를 받아 appleCnt 누적		
+	}
+
+	
+}
+class Seller{
+	int myMoney;
+	int appleCnt;
+	public int price;
+	Seller(int money,int cnt,int price){
+		this.myMoney = money;
+		this.appleCnt=cnt;
+		this.price = price;
+	}
+	public int receive(int money) {
+		myMoney+=money;			
+		int cnt=money/price;
+		appleCnt-=cnt;			
+		return cnt;				
+	};
+	
+}
+
+public class test {
+	
+	public static void main(String[] args) {
+		Buyer buyer01 = new Buyer(10000,0);
+//		Buyer buyer02 = new Buyer(10000,0);
+		
+		Seller seller01 = new Seller(100000,100,1000);
+		
+		buyer01.pay(seller01,3000);
+		
+		System.out.printf("%d,%d\n",buyer01.myMoney,buyer01.appleCnt );
+		System.out.printf("%d,%d\n",seller01.myMoney,seller01.appleCnt );
+		
+	}
+
+}
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
