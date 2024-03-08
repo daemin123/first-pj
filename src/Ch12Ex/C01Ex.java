7c3c295792b8457c9a4f7cdf8c06fa7af00b4e44
jungwoogyun <jwg8910@naver.com>
2024-03-08 12:39:19 +0900

    .

--- src/Ch12Ex/C01Ex.java
+++ src/Ch12Ex/C01Ex.java
@@ -10,7 +10,16 @@ class TV{
 	   public TV(int size) { this.size = size; }
 	   protected int getSize() { return size; }
 }
-
+class ColorTV extends TV{
+	int color;
+	public ColorTV(int size,int color) {
+		super(size);
+		this.color = color;
+	}
+	public void printProperty() {	
+		System.out.printf("%d인치 %d컬러\n", this.getSize(),this.color);
+	}
+}
 public class C01Ex {
 	
 	public static void main(String[] args) {
