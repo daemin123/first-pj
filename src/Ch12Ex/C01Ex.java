2d16d59c342a1313e1a9a7bf49c2d641de54e0d7
jungwoogyun <jwg8910@naver.com>
2024-03-08 11:17:57 +0900

    1

---
+++ src/Ch12Ex/C01Ex.java
@@ -0,0 +1,20 @@
+package Ch12Ex;
+
+
+//[1번] 다음 main() 메소드와 실행 결과를 참고하여 TV를 상속받은 ColorTV 클래스를 작성하라.
+
+//출력 결과
+//32인치 1024컬러
+class TV{
+	   private int size;
+	   public TV(int size) { this.size = size; }
+	   protected int getSize() { return size; }
+}
+
+public class C01Ex {
+	
+	public static void main(String[] args) {
+		   ColorTV myTV = new ColorTV(32, 1024);
+		   myTV.printProperty(); // 32인치 1024컬러 가 나오면 해결!
+	}
+}
